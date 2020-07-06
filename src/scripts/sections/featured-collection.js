02ef5befc97168dbd18a7697bad09de4c9de4257
Parallels <amir.ff@gmail.com>
2020-07-06 01:47:45 -0700

    Styled Featured Collection section withcarousel and Quick Add functionality.

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -7,6 +7,8 @@
  * @namespace featuredCollection
  */
 import {register} from '@shopify/theme-sections';
+import swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
 
 /**
  * Featured collection constructor
@@ -21,5 +23,71 @@ register('featured-collection', {
 
   publicMethod() {
     window.console.log('Initialising featured collection section');
+  },
+
+  onLoad: function() {
+
+    // Swiper Carousel
+    const mySwiper = new swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        320: {
+          slidesPerView:'auto',
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerGroup: 3,
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerGroup: 3,
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1440: {
+          slidesPerGroup: 4,
+          slidesPerView: 4,
+          spaceBetween: 22,
+        },
+      },
+      on: {
+        init: function () {
+          // show carousel after it is initialized
+          this.el.style.visibility = 'visible';
+        },
+      },
+    });
+
+    // AJAX Add To Cart
+    const cartButtons = document.getElementsByClassName("js-add-to-cart");
+    Array.from(cartButtons).forEach(button => {
+      button.addEventListener('click', (event) => { 
+        event.preventDefault();
+        fetch('/cart/add.js', {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Requested-With':'xmlhttprequest' 
+          },
+          body: JSON.stringify({'quantity': 1, 'id': button.dataset.variantId})
+        })
+        .then(function (data) {
+          console.log('Request succeeded with JSON response', data);
+        })
+        .catch(function (error) {
+          console.log('Request failed', error);
+        });
+      });
+    });
+    
   },
 });
